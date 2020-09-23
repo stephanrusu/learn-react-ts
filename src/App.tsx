5138c901b921dceb50d8c1f7e5c54557d9c2210d
Stefan Rusu <sru@miracle.dk>
2020-09-23 14:48:44 +0300

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import TodoPage from './components/TodoPage';
-import HomePage from './components/HomePage';
+import TodoPage from "./components/TodoPage";
+import HomePage from "./components/HomePage";
 
 // function App():JSX.Element {
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/todo'>
+        <Route exact path="/todo">
           <TodoPage />
         </Route>
-        <Route exact path='/'>
+        <Route exact path="/">
           <HomePage />
         </Route>
       </Switch>
     </Router>
   );
-};
+}
 
 export default App;
