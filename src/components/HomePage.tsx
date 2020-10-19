344d95f8f454fea0f99f6d297fdeb9c4a3858c89
Stefan Rusu <sru@miracle.dk>
2020-10-19 10:12:06 +0300

    Clear article page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ROUTE_ARTICLE, ROUTE_CALENDAR, ROUTE_ROOT, ROUTE_TODO } from '../router/routes';
+import { ROUTE_CALENDAR, ROUTE_TODO } from '../router/routes';
 
 function HomePage() {
   return (
@@ -17,9 +17,6 @@ function HomePage() {
               </div>
               <div className="control">
                 <Link to={ROUTE_CALENDAR} className="button is-primary is-outlined">Calendar</Link>
-              </div>
-              <div className="control">
-                <Link to={ROUTE_ARTICLE} className="button is-primary is-outlined">Article</Link>
               </div>
             </div>
           </div>
