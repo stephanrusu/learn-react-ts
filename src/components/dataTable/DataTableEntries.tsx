4cc1ceb622ac3c8f28b98ac667b7ced2e06dbf7c
Stefan Rusu <stephan.rusu@gmail.com>
2020-12-02 13:14:41 +0200

    Update pagination UI

--- src/components/dataTable/DataTableEntries.tsx
+++ src/components/dataTable/DataTableEntries.tsx
@@ -56,12 +56,6 @@ function DataTableEntries() {
           <th className="">
             <div className="th-wrap">
               <div className="is-relative">Genre</div>
-              <div className="filter-icon tag">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-sort-alt">
-                  <path d="M18 14L12 22L6 14H18Z"/>
-                  <path d="M18 10L12 2L6 10H18Z"/>
-                </svg>
-              </div>
             </div>
           </th>
         </tr>
--- src/components/dataTable/DataTablePagination.tsx
+++ src/components/dataTable/DataTablePagination.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 function DataTablePagination() {
   return (
-    <div className="card mt-4">
+    <div className="card mt-4 card-pagination">
       <div className="card-content">
         <nav className="pagination is-centered" role="navigation" aria-label="pagination">
-          <button className="pagination-previous">Previous</button>
-          <button className="pagination-next">Next page</button>
+          <button className="pagination-action pagination-previous">
+            <svg fill="none" height="24" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" className="feather"><polyline points="15 18 9 12 15 6"></polyline></svg>
+          </button>
+          <button className="pagination-action pagination-next">
+            <svg fill="none" height="24" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" className="feather"><polyline points="9 18 15 12 9 6"/></svg>
+          </button>
           <ul className="pagination-list">
             <li><button className="pagination-link" aria-label="Goto page 1">1</button></li>
             <li><span className="pagination-ellipsis">&hellip;</span></li>
@@ -17,6 +21,9 @@ function DataTablePagination() {
             <li><button className="pagination-link" aria-label="Goto page 86">86</button></li>
           </ul>
         </nav>
+        <div className="pagination-portal">
+
+        </div>
       </div>
     </div>
   );
